cc96a65b5b3e89ecbd0d2657e47df89665045b3e
Brian Gardner <brngardner@gmail.com>
2017-07-14 12:58:07 -0400

    Inject ReminderStore into AddReminderTest
    * This will use the TestReminderApplication so it will be the fake
    reminder store
    * Trigger the injection in the setup method
    * Clear the reminder store in the tear down method

--- app/src/androidTest/java/com/bignerdranch/android/testfriendlyarchitecture/controller/AddReminderTest.java
+++ app/src/androidTest/java/com/bignerdranch/android/testfriendlyarchitecture/controller/AddReminderTest.java
@@ -4,8 +4,15 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import com.bignerdranch.android.testfriendlyarchitecture.TestReminderApplication;
+import com.bignerdranch.android.testfriendlyarchitecture.model.store.ReminderStore;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -13,5 +20,17 @@ public class AddReminderTest {
     @Rule
     public ActivityTestRule<DrawerActivity> mActivityTestRule =
             new ActivityTestRule<>(DrawerActivity.class);
-    
+    @Inject
+    ReminderStore reminderStore;
+
+    @Before
+    public void setup() {
+        TestReminderApplication.get(mActivityTestRule.getActivity())
+                .getAppComponent().inject(this);
+    }
+
+    @After
+    public void tearDown() {
+        reminderStore.clearReminders();
+    }
 }
