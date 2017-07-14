918a91f351255eba61de68f4542380d0d458dfec
Brian Gardner <brngardner@gmail.com>
2017-07-14 11:47:01 -0400

    Inject view model into view holder

--- app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/controller/list/ReminderViewHolder.java
+++ app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/controller/list/ReminderViewHolder.java
@@ -3,18 +3,24 @@ package com.bignerdranch.android.testfriendlyarchitecture.controller.list;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 
+import com.bignerdranch.android.testfriendlyarchitecture.controller.ReminderApplication;
 import com.bignerdranch.android.testfriendlyarchitecture.databinding.ItemReminderBinding;
 import com.bignerdranch.android.testfriendlyarchitecture.model.Reminder;
 import com.bignerdranch.android.testfriendlyarchitecture.viewmodel.ReminderItemViewModel;
 
+import javax.inject.Inject;
+
 public class ReminderViewHolder extends RecyclerView.ViewHolder {
 
     private ItemReminderBinding binding;
+    @Inject
     ReminderItemViewModel viewModel;
 
     public ReminderViewHolder(ItemReminderBinding binding, Context context) {
         super(binding.getRoot());
+        ReminderApplication.get(context).getAppComponent().inject(this);
         this.binding = binding;
+        binding.setReminder(viewModel);
     }
 
     public void bindReminder(Reminder reminder) {
