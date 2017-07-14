44258ab1d99f20939567f499c8f39029bd5ec3f8
Brian Gardner <brngardner@gmail.com>
2017-07-14 11:16:10 -0400

    add missing import to date selection listener

--- app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/model/date/DateSelectionListener.java
+++ app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/model/date/DateSelectionListener.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.testfriendlyarchitecture.model.date;
 
+import org.joda.time.LocalDate;
+
 public interface DateSelectionListener {
     void onDateSelected(LocalDate date);
 }
