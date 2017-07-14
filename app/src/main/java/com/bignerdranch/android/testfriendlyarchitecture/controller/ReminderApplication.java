347ebbbea0ce7beaadb81d98b98b0b0e5f9a04a1
Brian Gardner <brngardner@gmail.com>
2017-07-14 11:30:20 -0400

    Add ReminderApplication
    * Setup dagger component
    * Setup applicaiton in manifest
    * Add Activity to manifest

--- app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/controller/ReminderApplication.java
+++ app/src/main/java/com/bignerdranch/android/testfriendlyarchitecture/controller/ReminderApplication.java
@@ -1,4 +1,39 @@
 package com.bignerdranch.android.testfriendlyarchitecture.controller;
 
-public class ReminderApplication {
+import android.app.Application;
+import android.content.Context;
+
+import com.bignerdranch.android.testfriendlyarchitecture.inject.AdapterModule;
+import com.bignerdranch.android.testfriendlyarchitecture.inject.AppComponent;
+import com.bignerdranch.android.testfriendlyarchitecture.inject.AppModule;
+import com.bignerdranch.android.testfriendlyarchitecture.inject.DaggerAppComponent;
+import com.bignerdranch.android.testfriendlyarchitecture.inject.StoreModule;
+import com.bignerdranch.android.testfriendlyarchitecture.inject.ViewModelModule;
+
+public class ReminderApplication extends Application {
+
+    protected AppComponent appComponent;
+
+    public static ReminderApplication get(Context context) {
+        return (ReminderApplication) context.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initializeDependencyInjection();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
+
+    protected void initializeDependencyInjection() {
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .viewModelModule(new ViewModelModule())
+                .storeModule(new StoreModule())
+                .adapterModule(new AdapterModule())
+                .build();
+    }
 }
